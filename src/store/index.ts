1b2415506bc994a01254dfb393fdf891f6fb1ef7
Nam <namtphann@gmail.com>
2023-01-12 23:30:33 +0100

    Add tailwindcss and pinia and configure eslint prettier pinia

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,7 @@
-import { createStore } from "vuex";
+import { defineStore } from "pinia";
 
-export default createStore({
-  state: {},
+export const useStore = defineStore("store", {
+  state: () => ({}),
   getters: {},
-  mutations: {},
   actions: {},
-  modules: {},
 });
