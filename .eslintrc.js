f0507ba7daabf27539afbb2d531b17f22ea291f5
Nam <namtphann@gmail.com>
2023-05-02 23:39:05 +0200

    Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,16 @@
 module.exports = {
-    parser: "vue-eslint-parser",
-    parserOptions: {
-        parser: "@typescript-eslint/parser",
-    },
-    env: {
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/vue3-recommended',
-        "prettier",
-        "@vue/typescript/recommended",
-    ],
-    rules: {
-        // override/add rules settings here, such as:
-        // 'vue/no-unused-vars': 'error'
-        'vue/multi-word-component-names': 'off',
-    },
-    plugins: ["@typescript-eslint", "prettier"],
-}
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
+  },
+  env: {
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier", "@vue/typescript/recommended", "plugin:storybook/recommended"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    'vue/multi-word-component-names': 'off'
+  },
+  plugins: ["@typescript-eslint", "prettier"]
+};
